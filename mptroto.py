e3e63cacac1d5c56e67174720ee19388c1da4b42
Ayden Tutkishbaev <157115448+ayden-tutkishbaev@users.noreply.github.com>
2024-10-12 19:54:01 +0500

    Update mptroto.py

--- mptroto.py
+++ mptroto.py
@@ -1,10 +1,13 @@
 from telethon import TelegramClient
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 client = TelegramClient('session_name', 26059888, "2f7d3e14e3f0ff8f86c7b402cd5e5f75",)
 
 
 async def get_chat_members(chat_id):
-    await client.start(bot_token='6716078059:AAHkjoM9qO-99g2UGAzceEZIx8gbUvMtUpo')
+    await client.start(bot_token=config['BOT_TOKEN'])
     members: list[dict] = []
     async for member in client.iter_participants(chat_id):
         members.append(
